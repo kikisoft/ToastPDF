a2157b66d048bb92c72534443af8e984c0a5ac97
kiki <kiki@itoast.kr>
2017-08-14 16:21:14 +0900

    프린터 드라이버 인스톨러

---
+++ ToastPDFInstaller/Registry.h
@@ -0,0 +1,70 @@
+#if !defined(AFX_REGKEY_H__E451D194_D4C3_11D1_AB17_0000E8425C3E__INCLUDED_)
+#define AFX_REGKEY_H__E451D194_D4C3_11D1_AB17_0000E8425C3E__INCLUDED_
+
+#include <winreg.h>
+#include <afxtempl.h>
+
+class Registry
+{
+
+// Construction
+public:
+	Registry();
+	Registry(Registry& regKey);
+	virtual ~Registry();
+
+	Registry& operator=(Registry regKey); 
+
+// Operations
+public:
+	// note: calling open on an already open key will assert
+	// you must call Close() first if you want to re-use Registry objects
+	LONG Open(HKEY hKeyRoot, LPCTSTR pszPath); 
+	void Close();
+	CString GetPath() const { return m_sPath; }
+	HKEY GetKey() const { return m_hKey; }
+	HKEY GetKeyRoot() const { return m_hKeyRoot; }
+		
+	LONG Write(LPCTSTR pszItem, DWORD dwVal);
+	LONG Write(LPCTSTR pszItem, LPCTSTR pszVal, DWORD dwType = REG_SZ);
+	LONG Write(LPCTSTR pszItem, const BYTE* pData, DWORD dwLength, DWORD dwType = REG_BINARY);
+		
+	LONG Read(LPCTSTR pszItem, DWORD& dwVal) const;
+	LONG Read(LPCTSTR pszItem, CString& sVal) const;
+	LONG Read(LPCTSTR pszItem, BYTE* pData, DWORD& dwLength) const;
+		
+	static LONG Delete(HKEY hKeyRoot, LPCTSTR pszPath);
+	static BOOL KeyExists(HKEY hKeyRoot, LPCTSTR pszPath);
+		
+	int GetSubkeyNames(CStringArray& aNames) const;
+	int GetValueNames(CStringArray& aNames) const;
+	BOOL HasValues() const;
+	DWORD GetValueType(LPCTSTR pszItem) const;
+		
+	BOOL ExportToIni(LPCTSTR szIniPath) const;
+	BOOL ImportFromIni(LPCTSTR szIniPath);
+		
+	static BOOL CopyKey(HKEY hkRootFrom, const CString& sFromPath, HKEY hkRootTo, const CString& sToPath);
+		
+		
+#ifndef _NOT_USING_MFC_
+	static CString GetAppRegPath(LPCTSTR szAppName = NULL);
+#endif
+
+protected:
+	HKEY 	m_hKey;
+	HKEY 	m_hKeyRoot;
+	CString m_sPath;
+
+protected:
+	static LONG RecurseDeleteKey(HKEY key, LPCTSTR lpszKey);
+	BOOL ExportKeyToIni(LPCTSTR pszKey, CStdioFile& file) const;
+	BOOL ExportValueToIni(DWORD nIndex, CStdioFile& file) const;
+	BOOL ImportSectionFromIni(const CString& sSection, CStdioFile& file, CString& sNextSection);	   
+
+	static BOOL CopyKeys(HKEY hkFrom, HKEY hkTo);
+	static BOOL CopyValues(HKEY hkFrom, HKEY hkTo);
+};
+
+#endif
+
---
+++ ToastPDFInstaller/ToastPDFInstaller.cpp
@@ -0,0 +1,179 @@
+// toast_vpdinst.cpp : Defines the entry point for the console application.
+//
+
+#include "precomp.h"
+#include <boost/regex.hpp>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/filesystem.hpp>
+
+#include "CCPrintInstallFunctions.h"
+#include "Registry.h"
+#include "ToastPDFInstaller.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// The one and only application object
+
+CWinApp theApp;
+
+using namespace std;
+using namespace boost::program_options;
+
+
+enum action
+{
+	NONE = 0,
+	INSTALL = 1,
+	UNINSTALL
+};
+
+/*  This custom option parse function recognize gcc-style
+option "-fbar" / "-fno-bar".
+*/
+pair<string, string> reg_foo(const string& s)
+{
+	if (s.find("-f") == 0) {
+		if (s.substr(2, 3) == "no-")
+			return make_pair(s.substr(5), string("false"));
+		else
+			return make_pair(s.substr(2), string("true"));
+	}
+	else {
+		return make_pair(string(), string());
+	}
+}
+
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+{
+    int nRetCode = 0;
+
+    HMODULE hModule = ::GetModuleHandle(nullptr);
+
+    if (hModule != nullptr)
+    {
+        // initialize MFC and print and error on failure
+        if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+        {
+            // TODO: change error code to suit your needs
+            wprintf(L"Fatal Error: MFC initialization failed\n");
+            nRetCode = 1;
+        }
+        else
+        {
+			options_description desc("Allowed options");
+			desc.add_options()("help,h", "produce a help message");
+			desc.add_options()("action,a", value<string>(), "action : install | uninstall");
+
+			variables_map vm;
+			store(command_line_parser(argc, argv).options(desc).extra_parser(reg_foo).run(), vm);
+
+			if (vm.empty()) {
+				return 0;
+			}
+
+			if (vm.count("help")) {
+				cout << desc << endl;
+				return 0;
+			}
+
+			if (vm["action"].empty()) {
+				return 0;
+			}
+
+			action act = action::NONE;
+
+			if (vm["action"].as<string>() == "install")
+			{
+				act = action::INSTALL;
+			}
+
+			if (vm["action"].as<string>() == "uninstall")
+			{
+				act = action::UNINSTALL;
+			}
+			
+			if (act == action::NONE)
+			{
+				cout << "action error!" << endl;
+				return -1;
+			}
+
+			if (act == action::INSTALL)
+			{
+				BOOL disableWow64FsRedirection = FALSE;
+				if (IsX64()) {
+					LPVOID oldValue;
+					BOOL bResult = Wow64DisableWow64FsRedirection(&oldValue);
+					disableWow64FsRedirection = TRUE;
+				}
+
+				CCPDFPrinterInstaller installer(NULL);
+				if (installer.DoInstall())
+				{
+					cout << "install success." << endl;
+				}
+				else
+				{
+					cout << "install failed." << endl;
+				}
+
+				if (disableWow64FsRedirection) {
+					LPVOID oldValue;
+					Wow64RevertWow64FsRedirection(&oldValue);
+				}
+
+				Registry registry;
+				if (registry.Open(HKEY_LOCAL_MACHINE, _T("SOFTWARE\\GPL Ghostscript\\9.16")) == ERROR_SUCCESS) {
+					CString gs_dll;
+					boost::filesystem::path dir(boost::filesystem::current_path());
+
+					gs_dll.Format(_T("%s\\gsdll32.dll"), dir.string());
+					registry.Write(_T("GS_DLL"), gs_dll);
+					CString gs_lib;
+					gs_lib.Format(_T("%s\\tools\\gs9.16\\bin;"), dir.string());
+					gs_lib.AppendFormat(_T("%s\\tools\\gs9.16\\lib;"), dir.string());
+					gs_lib.AppendFormat(_T("%s\\tools\\gs9.16\\fonts"), dir.string());
+					registry.Write(_T("GS_LIB"), gs_lib);
+					registry.Close();
+				}
+			}
+			else if (act == action::UNINSTALL)
+			{
+				BOOL disableWow64FsRedirection = FALSE;
+				if (IsX64()) {
+					LPVOID oldValue;
+					BOOL bResult = Wow64DisableWow64FsRedirection(&oldValue);
+					disableWow64FsRedirection = TRUE;
+				}
+
+				CCPDFPrinterInstaller installer(NULL);
+				if (installer.DoRemove())
+				{
+					cout << "uninstall success." << endl;
+				}
+				else
+				{
+					cout << "uninstall failed." << endl;
+				}
+
+				if (disableWow64FsRedirection) {
+					LPVOID oldValue;
+					Wow64RevertWow64FsRedirection(&oldValue);
+				}
+			}
+        }
+    }
+    else
+    {
+        // TODO: change error code to suit your needs
+        wprintf(L"Fatal Error: GetModuleHandle failed\n");
+        nRetCode = 1;
+    }
+
+    return nRetCode;
+}
